e0da47615e96fde5be5a739f3bffd0ca5df99b1d
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2024-02-03 00:57:22 +0200

    balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+size_t binary_tree_balance2(const binary_tree_t *tree,
+			    const binary_tree_t *tree2);
+
+/**
+ * binary_tree_balance2 - gets factor of tree
+ *
+ *@tree: param
+ *@tree2: param
+ *
+ *Return: returns factor
+ */
+
+size_t binary_tree_balance2(const binary_tree_t *tree,
+			    const binary_tree_t *tree2)
+{
+int cont1 = 0, cont2 = 0;
+
+if (tree == NULL)
+return (0);
+
+if (tree->left != NULL)
+cont1 += binary_tree_balance2(tree->left, tree2);
+
+if (tree->right != NULL)
+cont2 += binary_tree_balance2(tree->right, tree2);
+
+if (tree == tree2)
+return (cont1 - cont2);
+if (cont1 > cont2)
+return (cont1 + 1);
+return (cont2 + 1);
+}
+
+/**
+ * binary_tree_balance - gets tree
+ *
+ *@tree: param
+ *
+ *Return: returns factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (binary_tree_balance2(tree, tree));
+}
